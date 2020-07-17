48a3c60a34829ffc1386718b986048ca196a6332
Michael Neeley <micneeley14@gmail.com>
2020-07-16 22:20:15 -0400

    some css updates

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', "POST"])
 def login():
-    return 'Hello, World!'
+    return render_template("login.html")
