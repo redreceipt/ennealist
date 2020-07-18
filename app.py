23e57e89632dd6d37a04a338f2a78bcf97afe813
Michael Neeley <micneeley14@gmail.com>
2020-07-17 20:47:12 -0400

    heroku stuff

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
+import os
+
 from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 
 
 @app.route('/', methods=['GET', "POST"])
 def index():
-    if request.method == 'POST':
-        email = request.form["email"]
-        # TODO verify code
-        verified = True
-        if verified:
-            session['email'] = email
-        else:
-            # TODO do something if code is wrong
-            pass
     # TODO if email is in session, go to types page
-    # return render_template("types.html")
+    if "email" in session.keys():
+        return render_template("types.html")
     return render_template("login.html")
 
 
 @app.route('/verify', methods=["POST"])
 def verify():
+    email = request.form["email"]
+    # TODO verify code
+    # NOTE temp always true for debug
+    verified = "code" in request.form.keys()
+    if verified:
+        session['email'] = email
+        return redirect(url_for("index"))
+    # TODO flash message that code is invalid
     return render_template("verify.html", email=request.form["email"])
 
 
 @app.route('/logout')
 def logout():
-    # remove the email from the session if it's there
-    session.pop('username', None)
+    session.pop('email', None)
     return redirect(url_for('index'))
